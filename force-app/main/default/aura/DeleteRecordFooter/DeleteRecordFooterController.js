e498461d26d54ba76a2f2e384e9dc6b758d77eb5
Victor Degano <victordegano@gmail.com>
2021-12-09 23:42:26 -0300

    Action menu & delete modal refactor

---
+++ force-app/main/default/aura/DeleteRecordFooter/DeleteRecordFooterController.js
@@ -0,0 +1,33 @@
+({
+    handleCancel: function (component, event, helper) {
+        component.find("overlayLib").notifyClose();
+    },
+
+    handleDelete: function (component, event, helper) {
+        let record = component.get("v.record");
+        let sobjectLabel = component.get("v.sobjectLabel");
+
+        helper
+            .callApexController(component, "deleteRecord", { recordId: record.Id })
+            .then((result) =>
+                helper.launchToastAndClose(component, {
+                    time: 6000,
+                    title: "¡Success!",
+                    mode: "dismissible",
+                    message: `${sobjectLabel} "${record.Name}" was deleted.`,
+                    type: "success",
+                    icon: "success"
+                })
+            )
+            .catch((error) =>
+                helper.launchToastAndClose(component, {
+                    time: 7000,
+                    title: "Error!",
+                    mode: "pester",
+                    message: `${sobjectLabel} "${record.Name}" was not deleted.`,
+                    type: "error",
+                    icon: "error"
+                })
+            );
+    }
+});
