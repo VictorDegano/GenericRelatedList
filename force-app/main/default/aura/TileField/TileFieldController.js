b70bf8aab228152b98b53895fb87bb8e170aa099
Victor Mariano Degano <victor.degano@globant.com>
2022-04-03 00:03:33 -0300

    Fix tile related link issue

--- force-app/main/default/aura/TileField/TileFieldController.js
+++ force-app/main/default/aura/TileField/TileFieldController.js
@@ -3,10 +3,19 @@
         const column = component.get("v.column");
         const record = component.get("v.record");
 
+        const fieldValue = column.fieldName.includes("_LinkName")
+            ? window.location.href.substring(0, window.location.href.indexOf("com/")) + "com" + record[column.fieldName]
+            : record[column.fieldName];
+        const labelValue = column.fieldName.includes("_LinkName")
+            ? column.fieldName.includes("Case")
+                ? record[column.label].CaseNumber
+                : record[column.label].Name
+            : record[column.label.replace(" ", "")];
+
         LightningUtilities.setState(component, "v", {
-            fieldValue:
-                "LinkName" === column.fieldName ? record[column.fieldName].replace("/", "") : record[column.fieldName],
-            labelValue: record[column.label]
+            fieldValue: fieldValue,
+            fieldLabel: labelValue,
+            show: true
         });
     }
 });
